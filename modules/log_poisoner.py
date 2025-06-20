69dc4715aed4219dbaee53e61246d241ac2c6453
Hero24-00 <196810131+Hero24-x@users.noreply.github.com>
2025-06-20 09:46:57 +0530

    Update log_poisoner.py

--- modules/log_poisoner.py
+++ modules/log_poisoner.py
@@ -1,1 +1,13 @@
-
+def poison_logs():
+    fake_logs = [
+        "sudo apt install cowsay",
+        "cd ~/Downloads",
+        "ping google.com",
+        "nano my_diary.txt"
+    ]
+    bash_history = os.path.expanduser("~/.bash_history")
+    with open(bash_history, "a") as bh:
+        for log in fake_logs:
+            bh.write(f"{log}\n")
+    print("[+] Added fake commands to bash_history.")
+  
